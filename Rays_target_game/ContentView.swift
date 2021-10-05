ca9e0b3d35ea898341e9c712d2d044ede980e155
robert bulliard <bbulliard@gmail.com>
2021-10-05 16:38:06 -0500

    Initial Commit

---
+++ Rays_target_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rays_target_game
+//
+//  Created by Robert Bulliard on 10/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
