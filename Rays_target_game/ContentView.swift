fa3b905c40a45ffcdb965bc25345fbc07a47c80d
robert bulliard <bbulliard@gmail.com>
2021-10-06 10:04:23 -0500

    commit

--- Rays_target_game/ContentView.swift
+++ Rays_target_game/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+// import XCTest
 
 struct ContentView: View
 {
@@ -13,19 +14,27 @@ struct ContentView: View
     {
         VStack
         {
-            Text("🎯🎯🎯🎯\nPut the bullseye where the number goes")
+            Text("🎯🎯🎯🎯\nPut the bullseye where the number goes").bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+
             
             Text("89")
-            HStack {
+            HStack
+            {
                 Text("1")
                 Slider(value: .constant(50), in:
-                        1.0 ... 100.0)
+                        1.0 ... 100.0).font(.footnote)
                 Text("100")
             }
             
-            Button(action: {}) {
-               Text ("Hit me")
-            }
+            Button(action: {})
+               {
+                   Text ("Hit me please")
+                       .bold()
+               }
         }
 //
 //
@@ -37,6 +46,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
         ContentView()
-            .previewLayout(.fixed(width: 568, height: 320))
+            .previewLayout(.fixed(width: 468, height: 320))
     }
 }
