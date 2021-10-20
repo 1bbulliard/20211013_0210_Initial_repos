47bae41f3665cf46dab61fae0643e5119fc156ea
robert bulliard <bbulliard@gmail.com>
2021-10-19 22:09:26 -0500

    20211018_1815_works well

--- Rays_target_game/Models/game.swift
+++ Rays_target_game/Models/game.swift
@@ -9,7 +9,11 @@ import Foundation
 
 struct Game
 {
-    var target: Int = 42
+    var target: Int = Int.random(in: 1...100)
     var score: Int = 0
     var round: Int = 1
+    
+    func points(slidervalue: Int) -> Int {
+    return 999
+                                         }
 }
